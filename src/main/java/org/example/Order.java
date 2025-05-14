050bd028f8bf19ecd56c5326992c2a8c6e5aea75
Torben Jost <torbenjost@proton.me>
2025-05-14 16:08:38 +0200

    introduce OrderItem!

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -4,6 +4,6 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public  record Order(String id,
-                     List<Product> products,
+                     List<OrderItem> items,
                      BigDecimal totalPrice) {
 }
