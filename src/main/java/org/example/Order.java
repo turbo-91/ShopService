ef8c2a1568fcfe051f7ccb21c20eebf9fa7815d8
Torben Jost <torbenjost@proton.me>
2025-05-16 13:06:58 +0200

    add orderStatus to order

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -3,12 +3,14 @@ package org.example;
 import java.math.BigDecimal;
 import java.util.List;
 
-public  record Order(String id,
-                     List<OrderItem> items
+
+public  record Order(String id, List<OrderItem> items, OrderStatus orderStatus
 ) {
     public BigDecimal totalPrice() {
         return items.stream()
-                .map(item -> item.product().price().multiply(BigDecimal.valueOf(item.quantity())))
+                .map(item ->
+                        item.product().price()
+                                .multiply(BigDecimal.valueOf(item.quantity())))
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 }
