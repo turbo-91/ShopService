64526a71def6bc55f7713708e05b8df9a537995a
Torben Jost <torbenjost@proton.me>
2025-05-26 15:55:08 +0200

    add global exception handler

---
+++ src/main/java/org/shopservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package org.shopservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<String> handleProductNotFound(ProductNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    // Optional: Handle other exceptions here
+}
